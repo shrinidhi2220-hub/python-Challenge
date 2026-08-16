40489ed3da45c00cbe17183dec822d7c00c4b875
shrinidhi <shrinidhi2220@gmail.com>
2026-08-16 22:47:34 +0530

    Add variables and swapping example in variables.py

---
+++ Day1/variables.py
@@ -0,0 +1,24 @@
+name = "Shri"
+age = 19
+college = "Saptagiri NPS UNIVERSITY"
+branch = "CSE"
+semester = "2nd SEM"
+CGPA = 8.48
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
+print("Semester:", semester)
+print("CGPA:", CGPA)
+
+
+# Swapping two values
+a = 10
+b = 20
+
+print("Before swapping:", a, b)
+
+a, b = b, a
+
+print("After swapping:", a, b)
